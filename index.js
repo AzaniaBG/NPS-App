4eb4d48dd1ca3260cc31bae747bff40a97ee7d57
AzaniaBG <azaniazbaker@gmail.com>
2019-12-31 09:34:57 -0800

    working but not quite sure why

--- index.js
+++ index.js
@@ -5,8 +5,9 @@ const apiKey = "uUyGLv3QND480LOuhxVnB7Pvdd73YyBaQCJDCA69";
 const endpointURL = "https://developer.nps.gov/api/v1/parks";
  
 //format parms object into a propery query string
-function formatParameters(params) {
-    const queryItems = Object.keys(params).map(key=>`${encodeURIComponent(key)}=${params[key]}`);
+
+function formatParameters(params) { 
+    const queryItems = Object.keys(params).map(key=>`${encodeURIComponent(key)}=${encodeURIComponent(params[key])}`);
     return queryItems.join("&");
 }
 //Loop through returned JSON object and display relevant data in the DOM
@@ -25,17 +26,43 @@ console.log(responseJson);
     }
     $("#js-results").removeClass("hidden");
 }
+function displayMultiParksInfo(responseJson) {
+    let dataArr = responseJson.data;
+    let multipleStates = dataArr[0]["states"];
+console.log(`multipleStates is ${multipleStates}`)
+}
 
+function getMultiStateParks(multiSearch, maxResults) {
+    const params = {
+        api_key: apiKey,
+        limit: maxResults,
+    }
+    let search = `stateCode=${multiSearch}`
+    let queryString = formatParameters(params);
+    let url = endpointURL+"?"+queryString+search;
+//console.log(`url is ${url}`)
+    fetch(url).then(response => {
+        if(response.ok) {
+            return response.json();
+        }
+        throw new Error(response.statusText);
+    }).then(responseJson => {
+        console.log(`responseJson data is: `)
+        console.log(responseJson.data);
+        displayMultiParksInfo(responseJson)
+    }).catch(err => {
+        $("h2").append(`<p>OOPSIE POOPSIE! ${err.message}<p>`);
+        $("#js-error-message").removeClass("hidden");
+    })
+}
 //GET parks info w/ request to NPS API
-function getParksInfo(singleSearch, multSearch, maxResults) {
+function getStateInfo(singleSearch, maxResults) {
 //create an object to store search parameters
     const params = {
         api_key: apiKey,
         limit: maxResults,
         q: singleSearch,
-        stateCode: multSearch,
     }
-
     let queryString = formatParameters(params);
     let url = endpointURL+"?"+queryString;
 console.log(`url is ${url}`);
@@ -46,31 +73,40 @@ console.log(`url is ${url}`);
             return response.json();
         }
         throw new Error(response.statusText);
-    }).then(responseJson =>displayParkInfo(responseJson)).catch(err=> {
+    }).then(responseJson => displayParkInfo(responseJson)).catch(err=> {
         $("h2").append(`<p>OOPSIE POOPSIE! ${err.message}<p>`);
         $("#js-error-message").removeClass("hidden");
     });
     
 }
 function getInputValues() {
-    $("form").submit(event => {
+    $("form").submit("#input-value", event => {
         event.preventDefault();
-        let searchInput = $("#input-values").val();
-        let multSearchInput; 
-console.log(`searchInput is ${searchInput}`);
+        let searchInput = $("#input-value").val();
         let maxInput = $("#max-values").val();
-console.log(`maxInput is ${maxInput}`);
         if(searchInput.includes(",")) {
-            multSearchInput = searchInput.split(",");
-console.log(`multSearchInput is ${multSearchInput}`)
+            getMultiStateParks(searchInput, maxInput);
         }
-        getParksInfo(searchInput, multSearchInput, maxInput);
+console.log(`searchInput is ${searchInput}`);
+        
+        getStateInfo(searchInput, maxInput);
     });
+
+//     $("form").submit("#input-values", event => {
+//         event.preventDefault();
+//         let searchInput = $("#input-values").val();
+//         let maxInput = $("#max-values").val();
+// console.log(`searchInput is ${searchInput}`);
+//         getMultiStateParks(searchInput, maxInput);
+//     })
+
 }
 
 //when input submitted on form, get input values and store them in a variable; pass them to the GET function
 function watchForm() {
     getInputValues();
+    // getStateInfo(searchInput, maxInput);
+    // getMultiStateParks(searchInput, maxInput)
       
 }
 //once page loads, call watchForm functon
